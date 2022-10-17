d5b024d0c2eb63a4043e06ddb08004b0203a5fa0
DavidGetter1 <onet16@gmx.de>
2022-10-17 17:02:54 +0200

    created test chechIncidencia

---
+++ ZeppelinUMMartinezMierzwa/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import zeppelinum.ServicioGestionPlataforma;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ServicioGestionPlataforma sgp = new ServicioGestionPlataforma();
+		sgp.registrarRestaurante("krosse krabbe", 4);
+
+	}
+	
+}
--- ZeppelinUMMartinezMierzwa/src/test/java/pruebas/Test.java
+++ ZeppelinUMMartinezMierzwa/src/test/java/pruebas/Test.java
@@ -4,9 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.util.LinkedList;
+
+import persistencia.jpa.bean.Incidencia;
 import persistencia.jpa.bean.Restaurante;
 import persistencia.jpa.bean.TipoUsuario;
+import persistencia.jpa.bean.Usuario;
+import persistencia.jpa.dao.IncidenciaDAO;
 import persistencia.jpa.dao.RestauranteDAO;
+import persistencia.jpa.dao.UsuarioDAO;
 import zeppelinum.ServicioGestionPlataforma;
 
 class Test {
@@ -100,13 +105,31 @@ class Test {
 		Integer plato=servicio.nuevoPlato("plato1","", 10, restaurante_id);
 		
 		
-		
-		
-		
 		assertTrue(servicio.cambiarDisponibilidadPlato(plato, false));
 		
 	}
 	
+	@org.junit.jupiter.api.Test
+	void check(){
+		
+		// Create restaurant
+		// Create User
+		// Create Incidencia
+		// save incidencia
+		// incidencia.restaurant
+		Integer categoria_id=servicio.crearCategoria("cat1");
+		LinkedList<Integer> cats=new LinkedList<>();
+		cats.add(categoria_id);
+		Integer restaurante_id = servicio.registrarRestaurante("La Periquita",1,cats);
+		Integer plato = servicio.nuevoPlato("plato1","", 10, restaurante_id);
+		LocalDate fechaNacimiento = LocalDate.of(1990, 1, 8);
+		Integer usuario_id = servicio.registrarUsuario("Periquita", "Palotes", fechaNacimiento, "periquita@palotes.es",
+				"12345", TipoUsuario.RESTAURANTE);		
+		Integer incidencia = servicio.crearIncidencia(fechaNacimiento, "description", fechaNacimiento, "", usuario_id, restaurante_id);
+		Incidencia incidencia = IncidenciaDAO.getIncidenciaDAO().findById(incidencia);
+		assertNotNull(incidencia);
+		
+	}
 	
 	
 	
